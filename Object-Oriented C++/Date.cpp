3577d46852a303be2b3802ef7de644112e6aa3c4
PEI CHEN WU <peichenwu@PEIs-MBP.attlocal.net>
2022-07-20 23:38:14 -0700

    add polymorphism lab

---
+++ Object-Oriented C++/Date.cpp
@@ -0,0 +1,12 @@
+#include "Date.h"
+
+//Date Constructor
+Date::Date(int year, int month, int day) {
+    SetDate(year, month, day);
+}
+
+void Date::SetDate(int year, int month, int day) {
+    m_month = month;
+    m_day = day;
+    m_year = year;
+}
